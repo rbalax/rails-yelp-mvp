c52c85ce00401798618bfded8ccb962f1412f746
Rom Balax <rbalax@yahoo.co.uk>
2023-02-16 17:50:39 +0900

    added reviews and all test green

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,13 +13,16 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render restaurants_new_path
+    end
   end
 
   private
 
   def restaurant_params
-    require(:restaurant).permit(:name, :address, :category, :phone_number)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
   get "restaurants/index", to: "restaurants#index"
   get "restaurants/new", to: "restaurants#new"
   get "restaurants/:id", to: "restaurants#show"
   post "restaurants", to: "restaurants#create"
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # resources :restaurants
+
 end
